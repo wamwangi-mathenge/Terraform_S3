8db42bc0ee1bfb2b47c9cb5040447f901cc6ddb6
Brian <wamwangimathenge@gmail.com>
2023-10-28 18:14:16 +0300

    Display the bucket names as an output

---
+++ output.tf
@@ -0,0 +1,11 @@
+output "adventure_tech_bucket_output" {
+  value = aws_s3_bucket.adventure-tech.bucket
+}
+
+output "nature_escape_bucket_output" {
+  value = aws_s3_bucket.nature-escape.bucket
+}
+
+output "data_summit_bucket_output" {
+  value = aws_s3_bucket.data-summit.bucket
+}
