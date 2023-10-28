ba84f4a2834fb93606df32ac7b2ceff96a0d4378
Brian <wamwangimathenge@gmail.com>
2023-10-28 18:09:45 +0300

    Set the acl for all objects to private

--- main.tf
+++ main.tf
@@ -37,3 +37,18 @@ resource "aws_s3_bucket" "data-summit" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket_acl" "adventure-tech_acl" {
+    bucket = aws_s3_bucket.adventure-tech.id
+    acl = "private"
+}
+
+resource "aws_s3_bucket_acl" "nature_escape_acl" {
+  bucket = aws_s3_bucket.nature-escape.id
+  acl = "private"
+}
+
+resource "aws_s3_bucket_acl" "data-summit-acl" {
+  bucket = aws_s3_bucket.data-summit.id
+  acl = "private"
+}
